463494e35029634ba15f59ee294852930f7899d1
Catarina Lourenco <cubic.medusa@gmail.com>
2019-12-01 23:20:27 +0000

    refactored script

--- script.R
+++ script.R
@@ -1,11 +1,14 @@
-
-#setwd("C:/Users/Catarina/DIT/StatProg/ca2/statsprog-R-ca2")
-
 library(tibble)
 library(readr)
 library(dplyr)
 library(tidyr)
 library(magrittr)
+library(stringr)
+
+
+##############################
+## Loading files
+##############################
 
 # Load files into tibbles (using readr functions)
 # col_types(): Manually define data type of each column as necessary
@@ -29,58 +32,84 @@ clicks <- read_tsv("data/clicks.tsv",
                    col_types = 
                      cols(
                        campaign_id = col_character(),
-                       date = col_date(format = "%d/%m/%Y"), #date format needs to be specified because the original doesn't conform to ISO standard
-                       time = col_time()
-                      )
+                       time = col_time(format = "%H:%M:%S"),
+                       #date format needs to be specified because the original doesn't conform to ISO standard
+                       date = col_date(format = "%d/%m/%Y")
+
+                       
+                     )
 )
 
 impressions <- read_tsv("data/impressions.tsv", 
                         col_types = 
                           cols(
                             campaign_id = col_character(),
-                            date = col_date(format = "%d/%m/%Y"),
-                            time = col_time()
+                            time = col_time(format = "%H:%M:%S"),
+                            date = col_date(format = "%d/%m/%Y")
                           )
 )
 
-##############################################################################
+##############################
+## Utility functions
+##############################
 
 get_hour <- function(x){
+  
+  #' Gets the hours from a timestamp
+  #'
+  #' Takes a string with the correct time format, parses it and returns the hour element 
+  #'
+  #' x the input to be formatted
+
   format(strptime(x, "%H:%M:%S"),'%H')
 }
 
 get_mins_secs <- function(x){
+  
+  #' Gets the minutes and seconds from a timestamp
+  #'
+  #' Takes a string with the correct time format, parses it and returns the mins:secs elements 
+  #'
+  #' x the input to be formatted
+
   format(strptime(x, "%H:%M:%S"),'%M:%S')
 }
 
-new_clicks <- mutate(clicks, hours = format(strptime(clicks$time, "%H:%M:%S"),'%H'))
-# 
-# new_clicks <- mutate(new_clicks, hours2 = EST_to_UTC(new_clicks$hours))
-
-# EST_to_UTC <- function(x){
-#   result <- as.numeric(get_hour(x["time"])) + 5
-#   if(result %% 24){
-#     result <- (result %% 24)
-#   }
-#   return(str(result))
-# }
-
-# hours <- apply(new_clicks, 1, EST_to_UTC)
-# new_clicks$hours2 = hours
-
 time_format <- function(hour, x) {
-  if(hour >= 24){ #if the hour is 24 or more, then it restarts from 0 (and date is +1, handled elsewhere)
+  
+  #' Format hour and timestamp (24hours, "%H:%M:%S")
+  #'
+  #' Transform an integer into the correct hour (0 to 23),
+  #' concatenates the new value with the rest of the timestamp (minutes and seconds)
+  #' and returns the new timestamp with the correct hour format
+  #'
+  #' hour is the positive integer, x the row (used to get the seconds and minutes)
+  #' 
+
+  if(hour >= 24){ #if the hour is 24 or more, then it restarts from 0 (and date is +1, handled by convert_dates)
     hour <- (hour %% 24)
   }
-  new_time <- paste(str_pad(hour, width=2, pad="0"), get_mins_secs(x["time"]),  sep=":")
+  new_time <- paste(str_pad(hour, 
+                            width=2, pad="0"), #str_pad() adds leading zeros as necessary (ex.: 1 -> 01)
+                    get_mins_secs(x["time"]),  
+                    sep=":") #paste concatenates the two strings, hour and mins_secs, placing ":" between them
   return(new_time)
+  
 }
 
-# x is the row
-convert_timezone <- function(x){
+
+convert_time <- function(x){
+  
+  #' Convert EST and PST to UTC
+  #'
+  #' Adds the respective time difference to the timestamp
+  #'
+  #' x is the row, to be used in conjunction with apply()
+  #' 
+  
   hour <- x["time"]
-  if(x["timezone"] != "UTC"){
-    hour <- as.numeric(get_hour(hour))
+  if(x["timezone"] != "UTC"){ #enter if it's not UTC already
+    hour <- as.numeric(get_hour(hour)) # transform to numeric value to apply arithmetic operations
     if(x["timezone"] == "Eastern time"){
       hour <- hour + 5
     } 
@@ -89,53 +118,84 @@ convert_timezone <- function(x){
     }
     hour <- time_format(hour, x)
   }
-  return(hour)
+  return(hour) #returns transformed hour OR the same hour if already UTC
 }
 
+ 
 convert_dates <- function(x){
+  
+  #' Convert EST and PST dates to UTC
+  #'
+  #' Convert dates to next calendar day if the hour was transformed and went over 24h
+  #'
+  #' x is the row, to be used in conjunction with apply()
+  #'
+
   is_different_date <- get_hour(x["new_time"]) <  get_hour(x["time"])
-  return(ifelse(is_different_date, format(as.Date(x["date"]) + 1),  x["date"]))
+  return(ifelse(is_different_date,              #condition
+                format(as.Date(x["date"]) + 1), #result if true
+                x["date"])                      #result if false
+         ) 
+  
 }
 
-###################
-
-new_times <- apply(clicks, 1, convert_timezone)
-clicks2 <- mutate(clicks, new_time=new_times) 
-new_dates <- apply(clicks2, 1, convert_dates)
-clicks2$new_dates = new_dates
+##############################
+## Convert to UTC
+##############################
 
 
 
-new_times_i <- apply(impressions, 1, convert_timezone)
-impressions2 <- mutate(impressions, new_time=new_times_i) 
-new_dates_i <- apply(impressions2, 1, convert_dates)
-impressions2$new_dates = new_dates_i
+convert_to_UTC <- function(my_tibble) {
+  
+  #' Convert time, date and timezone columns to UTC
+  #'
+  #' Applies time and date transformations rowwise
+  #'
+  #' my_tibble is the tibble to be transformed
+  #'
+  
+  new_times <- apply(my_tibble, 1, convert_time) # the second argument, 1, indicates it's rowwise
+  temp_tibble <- mutate(my_tibble, new_time=new_times) 
+  new_dates <- apply(temp_tibble, 1, convert_dates)
+  temp_tibble$new_dates = new_dates
+  return(temp_tibble)
+}
 
-###################
+clicks2 <- convert_to_UTC(clicks)
+impressions2 <- convert_to_UTC(impressions)
 
-new_clicks <- mutate(clicks, 
-                     time=clicks2$new_time, 
-                     date=clicks2$new_dates, 
-                     timezone="UTC") %>% 
+##############################
+## Join advertisers and campaigns
+##############################
+
+extend_tbl <- function(orig_tibble, updated_tibble) {
+  
+  #' Creates a tibble with dates and times in UTC format, joins advertiser and campaign data 
+  #'
+  #' Puts converted times and date into date and time columns, 
+  #' joins with campaings and advertisers info
+  #'
+  #' orig_tibble is the original data, updated contains dates and times converted to UTC
+  #'
+  
+  
+  mutate(orig_tibble, 
+         time=updated_tibble$new_time, 
+         date=updated_tibble$new_dates, 
+         timezone="UTC") %>% 
   left_join(campaigns, by = c("campaign_id" = "id")) %>% 
   left_join(advertisers, by = c("advertiser_id" = "ID")) %>% 
   rename(campaign_name = name.x, advertiser_name = name.y)
+}
 
+new_clicks <- extend_tbl(clicks, clicks2)
 
+new_impressions <- extend_tbl(impressions, impressions2)
 
-new_impressions <- mutate(impressions, 
-                          time=impressions2$new_time, 
-                          date=impressions2$new_dates, 
-                          timezone="UTC") %>% 
-  left_join(campaigns, by = c("campaign_id" = "id")) %>% 
-  left_join(advertisers, by = c("advertiser_id" = "ID")) %>% 
-  rename(campaign_name = name.x, advertiser_name = name.y)
-
-###################
+##############################
+## Save to csv
+##############################
 
 write_csv(new_impressions, "impressions_processed.csv")
 write_csv(new_clicks, "clicks_processed.csv")
-
-###########################################################################
-
 
